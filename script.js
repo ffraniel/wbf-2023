2490497028be992e79407bf9d220388c25bf5370
ffraniel <francis.whitehead@googlemail.com>
2023-11-23 02:33:12 +0000

    responsive and navigation menu

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("you shithead")
+const closeNavBtn = document.querySelector(".close-menu");
+const openNavBtn = document.querySelector(".menu-btn");
+const navigationMenuList = document.querySelector(".nav-list"); 
+const links = document.querySelectorAll(".links");
+
+closeNavBtn.addEventListener('click', toggleMenu);
+
+links.forEach(link => {
+  console.log("one more")
+  link.addEventListener('click', toggleMenu);
+})
+
+openNavBtn.addEventListener('click', toggleMenu);
+
+
+function toggleMenu (e) {
+  // check if screen is wider than 720px and if so do it, if not ignorec
+  console.log("window size, ", window.screen.width);
+  navigationMenuList.classList.toggle("open");
+} 
